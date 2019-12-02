6dcca225a2b53a85f17cf86644f7849382541568
skr1p7 <madhu1601gautam@gmail.com>
2019-12-03 01:08:37 +0530

    bruteforcer

---
+++ brute.py
@@ -0,0 +1,16 @@
+import zipfile 
+ 
+myfile = 'myfile.zip' 
+w_list = 'list.txt' 
+ 
+password = None 
+file = zipfile.ZipFile(myfile) 
+with open(w_list, 'r') as f: 
+   for line in f.readlines(): 
+         password = line.strip('\n') 
+         try: 
+               file.extractall(pwd=password) 
+               password = 'Pass is : %s' % password 
+               print password 
+         except: 
+               pass 
